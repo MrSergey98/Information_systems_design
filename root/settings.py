b9bfa786212c2da4060df6dfa3c5026c6d5cf80f
MrSergey <mrsergey98@mail.ru>
2024-11-24 13:34:45 +0300

    feat(LR4): add jinja_env in settings

--- root/settings.py
+++ root/settings.py
@@ -1,4 +1,15 @@
 from pathlib import Path
+import jinja.src.jinja2 as jinja2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+jinja_env.globals['fields'] = {
+    'email' : 'Email',
+    'firstname': 'Имя',
+    'surname': 'Фамилия',
+    'fathersname': 'Отчество',
+    'phone_number': 'Номер телефона',
+    'pasport': 'Паспорт',
+    'balance': 'Баланс',
+}
